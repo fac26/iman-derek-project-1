da182c8ebb3a0bfc1c655b97c798a8e0d1942e72
ll835 <loktinlo@gmail.com>
2022-10-01 19:16:18 +0100

    allow users to change slides in carousel via indicators at the bottom

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const nextSlideBtn = document.querySelector(".next-slide");
 const previousSlideBtn = document.querySelector(".previous-slide");
 nextSlideBtn.addEventListener("click", () => {
-    changeSlides(1)
+    changeSlides(1);
 }
 )
 previousSlideBtn.addEventListener("click", () => {
-    changeSlides(-1)
+    changeSlides(-1);
 }
 )
 function changeSlides(direction){
@@ -15,10 +15,31 @@ function changeSlides(direction){
     const arr = Array.from(allSlides);
     let newIndex = arr.indexOf(currentSlide) + direction;
     if (newIndex < 0){
-        newIndex = arr.length - 1
+        newIndex = arr.length - 1;
     }
     if (newIndex > arr.length - 1){
         newIndex = 0;
     }
     arr[newIndex].dataset.activeSlide = true;
+    const currentIndicator = document.querySelector("[data-active-indicator]");
+    delete currentIndicator.dataset.activeIndicator;
+    const slideIndicators = document.querySelectorAll(".slide-indicator");
+    const indicatorArr = Array.from(slideIndicators);
+    indicatorArr[newIndex].dataset.activeIndicator = true;
+}
+const slideIndicators = document.querySelectorAll(".slide-indicator");
+slideIndicators.forEach(indicator => indicator.addEventListener(("click"), (e) => {
+    changeSlidesViaIndicators(e.target);
+}))
+function changeSlidesViaIndicators(indicator){
+    const currentIndicator = document.querySelector("[data-active-indicator]");
+    const currentSlide = document.querySelector("[data-active-slide]");
+    const allSlides = document.querySelectorAll(".slide");
+    const arr = Array.from(allSlides);
+    const indicatorArr = Array.from(slideIndicators);
+    const newIndicatorindex = indicatorArr.indexOf(indicator);
+    delete currentIndicator.dataset.activeIndicator;
+    indicator.dataset.activeIndicator = true;
+    delete currentSlide.dataset.activeSlide;
+    arr[newIndicatorindex].dataset.activeSlide = true;
 }
